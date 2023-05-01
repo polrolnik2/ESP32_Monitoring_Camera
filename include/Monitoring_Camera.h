1351cb880eef805c15093b24c29b2f9dae8b5854
polrolnik2 <45858767+polrolnik2@users.noreply.github.com>
2023-05-01 14:27:11 +0200

    top include files creation

---
+++ include/Monitoring_Camera.h
@@ -0,0 +1,8 @@
+#ifndef __MONITORING_CAMERA_H___
+#define __MONITORING_CAMERA_H___
+
+class Monitoring_Camera {
+
+};
+
+#endif // __MONITORING_CAMERA_H___
---
+++ include/monitoring_camera_servo_control/monitoring_camera_servo_control.h
@@ -0,0 +1,61 @@
+#ifndef __MONITORING_CAMERA_SERVO_CONTROL_H___
+#define __MONITORING_CAMERA_SERVO_CONTROL_H___
+
+#include <ESP32Servo.h>
+
+class monitoring_camera_servo_control
+{
+private:
+    int x_servo_pin;
+    int y_servo_pin;
+    Servo X_Servo;
+    Servo Y_Servo;
+public:
+    monitoring_camera_servo_control(int x_servo_pin, int y_servo_pin, int PeriodHertz);
+    ~monitoring_camera_servo_control();
+
+    void x_servo_add(int step);
+    void y_servo_add(int step);
+    void x_servo_sub(int step);
+    void y_servo_sub(int step);
+    void x_servo_set(int angle);
+    void y_servo_set(int angle);
+};
+
+monitoring_camera_servo_control::monitoring_camera_servo_control(int x_servo_pin, int y_servo_pin, int PeriodHertz = 50)
+{
+    X_Servo.setPeriodHertz(PeriodHertz);
+    X_Servo.attach(x_servo_pin);
+    Y_Servo.setPeriodHertz(PeriodHertz);
+    Y_Servo.attach(y_servo_pin);
+}
+
+monitoring_camera_servo_control::~monitoring_camera_servo_control()
+{
+}
+
+inline void monitoring_camera_servo_control::x_servo_add(int step) {
+    X_Servo.write(X_Servo.read() + step);
+}
+
+inline void monitoring_camera_servo_control::y_servo_add(int step) {
+    Y_Servo.write(Y_Servo.read() + step);
+}
+
+inline void monitoring_camera_servo_control::x_servo_sub(int step) {
+    X_Servo.write(X_Servo.read() - step);
+}
+
+inline void monitoring_camera_servo_control::y_servo_sub(int step) {
+    Y_Servo.write(Y_Servo.read() - step);
+}
+
+inline void monitoring_camera_servo_control::x_servo_set(int angle) {
+    X_Servo.write(angle);
+}
+
+inline void monitoring_camera_servo_control::y_servo_set(int angle) {
+    Y_Servo.write(angle);
+}
+
+#endif // __MONITORING_CAMERA_SERVO_CONTROL_H___
