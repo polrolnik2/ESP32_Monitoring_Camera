299d8b8918f9ffffdac8303f67c3e5445f6a2c8b
polrolnik2 <45858767+polrolnik2@users.noreply.github.com>
2023-05-25 21:29:04 +0200

    configuration fixes - disable dtr, rts

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <Arduino.h>
+#include <Monitoring_Camera.h>
+
+monitoring_camera_servo_control ServoControl(_x_servo_pin, _y_servo_pin);
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+    SetServoControl(&ServoControl);
+    web_server_init("Kaplica", "Krzysiek2012");
 }
 
 void loop() {
