078f0a6cefb950cab392aa27486c88c34b690b2e
polrolnik2 <45858767+polrolnik2@users.noreply.github.com>
2023-05-29 15:00:49 +0200

    Coonfiguration changes for esp32 cam

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,9 @@ monitoring_camera_servo_control ServoControl(_x_servo_pin, _y_servo_pin);
 
 void setup() {
     Serial.begin(115200);
+    Serial.println("Monitoring Camera");
     SetServoControl(&ServoControl);
-    web_server_init("Kaplica", "Krzysiek2012");
+    web_server_init(_SSID, _PSK);
 }
 
 void loop() {
